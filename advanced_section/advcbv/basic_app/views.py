4a843b2075017e10279ff33083547596c0c7446f
Untitled <john.d.reddock@gmail.com>
2018-06-07 14:48:23 -0400

    list and detail view 3

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView,ListView,DetailView
 from . import models
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
 class SchoolListView(ListView):
     context_object_name = 'school_listy'
     model = models.School
@@ -10,5 +13,3 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detaily'
     model = models.School
     template_name = 'basic_app/school_detail.html'
-
-
