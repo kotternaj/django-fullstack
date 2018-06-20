d0fb9318b1a2db8034dddfe3937f0ff5d2190c71
Untitled <john.d.reddock@gmail.com>
2018-06-20 17:17:58 -0400

    itemList

--- flask-rest-api/app2.py
+++ flask-rest-api/app2.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app2 = Flask(__name__)
@@ -14,11 +14,17 @@ class Item(Resource):
         return {'item': None}, 404
     
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
-    
-api.add_resource(Item, '/item/<string:name>') # http//localhost:5001/student/Rolf
-    
-app2.run(port=5001)
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items/')
+
+app2.run(port=5001, debug=True)
 
