a340a0e2fb713511adcae61cf7a7cb7c588cb92f
Untitled <john.d.reddock@gmail.com>
2018-06-20 16:41:28 -0400

    post endpoint & status codes

--- flask-rest-api/app2.py
+++ flask-rest-api/app2.py
@@ -4,11 +4,21 @@ from flask_restful import Resource, Api
 app2 = Flask(__name__)
 api = Api(app2)
 
-class Student(Resource):
-    def get(self,name):
-        return {'student': name }
+items = []
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
     
-api.add_resource(Student, '/student/<string:name>') # http//localhost:5001/student/Rolf
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201
+    
+api.add_resource(Item, '/item/<string:name>') # http//localhost:5001/student/Rolf
     
 app2.run(port=5001)
 
