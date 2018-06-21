5be289b8acdb67969bb062f0328f4ebf57d2fde2
Untitled <john.d.reddock@gmail.com>
2018-06-20 21:42:00 -0400

    authentication JWT

--- flask-rest-api/app2.py
+++ flask-rest-api/app2.py
@@ -1,23 +1,46 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app2 = Flask(__name__)
+app2.secret_key = 'john'
 api = Api(app2)
+
+jwt = JWT(app2, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+    # @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
     
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': 'An item with this name {} already exists'.format(name)},400
+        
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+    
+    def delete(self,name):
+        global items 
+        item = list(filter(lambda x: x['name'] !=name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
